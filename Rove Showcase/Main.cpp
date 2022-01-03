8f2cdc4241f44fdb16fc7e33f2f43ef9c0f55d86
Callum Anning <CallumAnning@hotmail.co.uk>
2022-01-03 20:15:57 +0000

    Added Pch

--- Rove Showcase/Main.cpp
+++ Rove Showcase/Main.cpp
@@ -1,34 +1,8 @@
 
-#define WIN32_LEAN_AND_MEAN
-#include <Windows.h>
+#include "Pch.h"
 
 static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	// Get window pointer
-	//TWindow* window = nullptr;
-	//if (uMsg == WM_NCCREATE)
-	//{
-	//	CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
-	//	window = static_cast<TWindow*>(pCreate->lpCreateParams);
-	//	SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)window);
-
-	//	window->m_Hwnd = hwnd;
-	//}
-	//else
-	//{
-	//	window = reinterpret_cast<TWindow*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-	//}
-
-	//// Loop
-	//if (window != nullptr)
-	//{
-	//	return window->HandleMessage(uMsg, wParam, lParam);
-	//}
-	//else
-	//{
-	//	return DefWindowProc(hwnd, uMsg, wParam, lParam);
-	//}
-
 	switch (uMsg)
 	{
 	case WM_CREATE:
@@ -40,9 +14,10 @@ static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM l
 
 	case WM_SIZE:
 		return 0;
-	}
 
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	default:
+		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
