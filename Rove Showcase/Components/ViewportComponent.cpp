e695687c2ba7d02b6252877a652f09de84266784
Callum Anning <CallumAnning@hotmail.co.uk>
2022-01-07 19:11:54 +0000

    Refactored viewport

--- Rove Showcase/Components/ViewportComponent.cpp
+++ Rove Showcase/Components/ViewportComponent.cpp
@@ -8,66 +8,13 @@ Rove::ViewportComponent::ViewportComponent(Application* application) : m_Applica
 
 void Rove::ViewportComponent::OnCreate()
 {
-	// Texture
-	auto device =  m_Application->GetRenderer()->GetDevice();
-	auto context = m_Application->GetRenderer()->GetDeviceContext();
-
 	int width, height;
 	m_Application->GetWindow()->GetSize(&width, &height);
 
-	// Render targets
-	{
-		D3D11_TEXTURE2D_DESC texture_desc = {};
-		texture_desc.Width = width;
-		texture_desc.Height = height;
-		texture_desc.MipLevels = 1;
-		texture_desc.ArraySize = 1;
-		texture_desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-		texture_desc.SampleDesc.Count = 1;
-		texture_desc.SampleDesc.Quality = 0;
-		texture_desc.Usage = D3D11_USAGE_DEFAULT;
-		texture_desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
-		texture_desc.CPUAccessFlags = 0;
-		texture_desc.MiscFlags = 0;
-
-		DX::Check(device->CreateTexture2D(&texture_desc, 0, m_Texture.ReleaseAndGetAddressOf()));
-
-		// Create the render target view.
-		D3D11_RENDER_TARGET_VIEW_DESC target_view_desc = {};
-		target_view_desc.Format = texture_desc.Format;
-		target_view_desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
-		target_view_desc.Texture2D.MipSlice = 0;
-
-
-		DX::Check(device->CreateRenderTargetView(m_Texture.Get(), &target_view_desc, m_TextureRenderTargetView.ReleaseAndGetAddressOf()));
-	}
-
-	// Depth stencil
-	{
-		D3D11_TEXTURE2D_DESC texture_desc = {};
-		texture_desc.Width = width;
-		texture_desc.Height = height;
-		texture_desc.MipLevels = 1;
-		texture_desc.ArraySize = 1;
-		texture_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
-		texture_desc.SampleDesc.Count = 1;
-		texture_desc.SampleDesc.Quality = 0;
-		texture_desc.Usage = D3D11_USAGE_DEFAULT;
-		texture_desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
-
-		ComPtr<ID3D11Texture2D> texture = nullptr;
-		DX::Check(device->CreateTexture2D(&texture_desc, nullptr, texture.ReleaseAndGetAddressOf()));
-		DX::Check(device->CreateDepthStencilView(texture.Get(), nullptr, m_TextureDepthStencilView.ReleaseAndGetAddressOf()));
-	}
-
-	// Shader resource
-	D3D11_SHADER_RESOURCE_VIEW_DESC view_desc = {};
-	view_desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
-	view_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
-	view_desc.Texture2D.MostDetailedMip = 0;
-	view_desc.Texture2D.MipLevels = 1;
-
-	DX::Check(device->CreateShaderResourceView(m_Texture.Get(), &view_desc, m_RenderedTexture.ReleaseAndGetAddressOf()));
+	CreateSharedTexture(width, height);
+	CreateRenderTargetView();
+	CreateDepthStencilView(width, height);
+	CreateShaderResourceView();
 }
 
 void Rove::ViewportComponent::OnRender()
@@ -77,8 +24,8 @@ void Rove::ViewportComponent::OnRender()
 
 	ImVec2 pos = ImGui::GetCursorScreenPos();
 	ImVec2 size = ImGui::GetContentRegionAvail();
-	int width = (int)size.x;
-	int height = (int)size.y;
+	int width = static_cast<int>(size.x);
+	int height = static_cast<int>(size.y);
 
 	ImGui::GetWindowDrawList()->AddImage(reinterpret_cast<void*>(m_RenderedTexture.Get()), pos, ImVec2(pos.x + width, pos.y + height));
 
@@ -95,4 +42,74 @@ void Rove::ViewportComponent::Set()
 
 	// Bind the render target view to the pipeline's output merger stage
 	context->OMSetRenderTargets(1, m_TextureRenderTargetView.GetAddressOf(), m_TextureDepthStencilView.Get());
+}
+
+void Rove::ViewportComponent::CreateSharedTexture(int width, int height)
+{
+	auto device = m_Application->GetRenderer()->GetDevice();
+
+	// Texture
+	D3D11_TEXTURE2D_DESC texture_desc = {};
+	texture_desc.Width = width;
+	texture_desc.Height = height;
+	texture_desc.MipLevels = 1;
+	texture_desc.ArraySize = 1;
+	texture_desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	texture_desc.SampleDesc.Count = 1;
+	texture_desc.SampleDesc.Quality = 0;
+	texture_desc.Usage = D3D11_USAGE_DEFAULT;
+	texture_desc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	texture_desc.CPUAccessFlags = 0;
+	texture_desc.MiscFlags = 0;
+
+	DX::Check(device->CreateTexture2D(&texture_desc, 0, m_SharedTexture.ReleaseAndGetAddressOf()));
+}
+
+void Rove::ViewportComponent::CreateShaderResourceView()
+{
+	auto device = m_Application->GetRenderer()->GetDevice();
+
+	D3D11_SHADER_RESOURCE_VIEW_DESC view_desc = {};
+	view_desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	view_desc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+	view_desc.Texture2D.MostDetailedMip = 0;
+	view_desc.Texture2D.MipLevels = 1;
+
+	DX::Check(device->CreateShaderResourceView(m_SharedTexture.Get(), &view_desc, m_RenderedTexture.ReleaseAndGetAddressOf()));
+}
+
+void Rove::ViewportComponent::CreateRenderTargetView()
+{
+	auto device = m_Application->GetRenderer()->GetDevice();
+	auto context = m_Application->GetRenderer()->GetDeviceContext();
+
+	// Create the render target view.
+	D3D11_RENDER_TARGET_VIEW_DESC target_view_desc = {};
+	target_view_desc.Format = DXGI_FORMAT_R32G32B32A32_FLOAT;
+	target_view_desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	target_view_desc.Texture2D.MipSlice = 0;
+
+	DX::Check(device->CreateRenderTargetView(m_SharedTexture.Get(), &target_view_desc, m_TextureRenderTargetView.ReleaseAndGetAddressOf()));
+}
+
+void Rove::ViewportComponent::CreateDepthStencilView(int width, int height)
+{
+	auto device = m_Application->GetRenderer()->GetDevice();
+	auto context = m_Application->GetRenderer()->GetDeviceContext();
+
+	// Create depth stencil
+	D3D11_TEXTURE2D_DESC texture_desc = {};
+	texture_desc.Width = width;
+	texture_desc.Height = height;
+	texture_desc.MipLevels = 1;
+	texture_desc.ArraySize = 1;
+	texture_desc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	texture_desc.SampleDesc.Count = 1;
+	texture_desc.SampleDesc.Quality = 0;
+	texture_desc.Usage = D3D11_USAGE_DEFAULT;
+	texture_desc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+
+	ComPtr<ID3D11Texture2D> texture = nullptr;
+	DX::Check(device->CreateTexture2D(&texture_desc, nullptr, texture.ReleaseAndGetAddressOf()));
+	DX::Check(device->CreateDepthStencilView(texture.Get(), nullptr, m_TextureDepthStencilView.ReleaseAndGetAddressOf()));
 }
--- Rove Showcase/Components/ViewportComponent.h
+++ Rove Showcase/Components/ViewportComponent.h
@@ -19,10 +19,20 @@ namespace Rove
 	private:
 		Application* m_Application = nullptr;
 
-		ComPtr<ID3D11ShaderResourceView> m_RenderedTexture = nullptr;
-		ComPtr<ID3D11Texture2D> m_Texture = nullptr;
+		// Texture used for both shader resource and render target
+		ComPtr<ID3D11Texture2D> m_SharedTexture = nullptr;
+		void CreateSharedTexture(int width, int height);
 
+		// Shader resource view
+		ComPtr<ID3D11ShaderResourceView> m_RenderedTexture = nullptr;
+		void CreateShaderResourceView();
+
+		// Render target view
 		ComPtr<ID3D11RenderTargetView> m_TextureRenderTargetView = nullptr;
+		void CreateRenderTargetView();
+
+		// Depth stencil view
 		ComPtr<ID3D11DepthStencilView> m_TextureDepthStencilView = nullptr;
+		void CreateDepthStencilView(int width, int height);
 	};
 }
